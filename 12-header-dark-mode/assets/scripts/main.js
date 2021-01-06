3fe5fa39245170bb1673bb420e159b4eacffca42
Samir Valiyev <samir.valiyev@gmail.com>
2021-01-06 18:13:40 +0400

    added responsiv menu

--- 12-header-dark-mode/assets/scripts/main.js
+++ 12-header-dark-mode/assets/scripts/main.js
@@ -1,5 +1,20 @@
+// dark mode toggle starts
 const toggleMenu = document.querySelector(".toggle-state");
 
 toggleMenu.addEventListener("click", function () {
 	document.body.classList.toggle("dark-mode");
 });
+// dark mode toggle ends
+
+// header navigation menu stats
+const bars = document.querySelector(".menu-animation-bars");
+const navMenu = document.querySelector(".nav");
+
+bars.addEventListener("click", function () {
+	if (navMenu.style.display === "block") {
+		navMenu.style.display = "none";
+	} else {
+		navMenu.style.display = "block";
+	}
+});
+// header navigation menu ends
