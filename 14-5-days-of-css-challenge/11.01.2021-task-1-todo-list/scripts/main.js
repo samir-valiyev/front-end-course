12856f3330681d3e74432ead259402fc6cd877ab
Samir Valiyev <samir.valiyev@gmail.com>
2021-01-13 18:18:47 +0400

    task-1 added javascript

--- 14-5-days-of-css-challenge/11.01.2021-task-1-todo-list/scripts/main.js
+++ 14-5-days-of-css-challenge/11.01.2021-task-1-todo-list/scripts/main.js
@@ -1,3 +1,4 @@
+"use strict";
 /*============ Date starts ============*/
 const fullDate = document.querySelector(".full-date");
 const monthNames = [
@@ -16,19 +17,25 @@ const monthNames = [
 ];
 
 const now = new Date();
-const month = `${monthNames[now.getMonth()]}`;
 const date = `${now.getDate()}`;
+const month = `${monthNames[now.getMonth()]}`;
 const year = `${now.getFullYear()}`;
 
 fullDate.innerHTML = `${month} ${date}, ${year}`;
 /*========== Date ends =========== */
 
 /*========== Todo list starts =========== */
-
 const ul = document.querySelector(".todo-list__items");
-const li = document.querySelector(".todo-list__item");
-const i = document.querySelector("#i");
-ul.addEventListener("click", (event) => {
-	event.target.classList.toggle("checked");
-});
+const li = document.querySelectorAll(".todo-list__item");
+const icon = document.querySelectorAll("i");
+for (let i = 0; i < li.length; i++) {
+	li[i].addEventListener("click", function () {
+		this.classList.toggle("checked");
+		this.children[0].classList.toggle("far");
+		this.children[0].classList.toggle("fa-circle");
+		this.children[0].classList.toggle("fas");
+		this.children[0].classList.toggle("fa-check");
+	});
+}
+
 /*========== Todo list ends =========== */
